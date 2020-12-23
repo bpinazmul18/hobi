ca2f8631772d28c791cf1031356c2e417ad811fb
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-12-23 15:43:30 +0600

    design education section

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,27 @@
     "use strict";
     // your code here
     animate();
+
+
+    //Isotop
+    var $grid = $('.grid').isotope({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        masonry: {
+            // use outer width of grid-sizer for columnWidth
+            columnWidth: 1
+        }
+    })
+
+// filter items on button click
+    $('.filter-button-group').on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+
+
+
+
 })(jQuery);
 //ANIMATED PROGRESS BAR
 function animate() {
