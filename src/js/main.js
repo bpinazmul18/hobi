552279ea5f8be9ac9641795aece89dc5a7dc95be
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-12-24 12:50:49 +0600

    add map api and counter section and counter up intregation

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
     "use strict";
     // your code here
     animate();
+    //MAP INITIATE
+    myMaps();
 
 
     //Isotop
@@ -21,7 +23,8 @@
     });
 
 
-
+    // COUNTER UP
+    $('.counter').countUp();
 
 })(jQuery);
 //ANIMATED PROGRESS BAR
@@ -40,4 +43,24 @@ function animate() {
             }
         });
     });
+}
+
+
+//LEAFLET JS MAPS
+function myMaps() {
+    var L = window.L;
+    var mymap = L.map('map',{ zoomControl: false }).setView([51.509865, -0.118092], 13);
+    var markerIcon = L.icon({
+        iconUrl: './img/marker_logo.png',
+        iconSize: [60, 60],
+    });
+    var marker = L.marker([51.509865, -0.118092], { icon: markerIcon }).addTo(mymap);
+    marker.bindPopup("<h3>Adrress:</h3> <p>450 Strand, Charing Cross, London WC2R 0RG, UK </p><h3>Phone:</h3>+44 20 7930 8205  ||  +44 20 7839 2323");
+
+    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=4s5tSx5WUxHw28DRO8fi', {
+        attribution: '<a href="https://api.mapbox.com/styles/v1/devnazmul20/ckj2e1tph1ivs19mwgxmz3pc5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2bmF6bXVsMjAiLCJhIjoiY2tqMmRob29jMDJmZzJzazNpanVuZnk5ZiJ9.mywcRlchbp6szcofExXdtA" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank"></a>',
+        minZoom: 3,
+        tileSize: 512,
+        zoomOffset: -1,
+    }).addTo(mymap);
 }
