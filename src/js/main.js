6a6d9f4eea6f94a038c9e0a6068fc41452da3dab
Md Nazmul Haque <48866588+bpinazmul18@users.noreply.github.com>
2020-12-24 16:37:14 +0600

    add testimonial section and owl-carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,11 @@
 (function($) {
     "use strict";
     // your code here
+    //ANIMATED PROGRESS BAR
     animate();
+
     //MAP INITIATE
     myMaps();
-
-
-    //Isotop
-    var $grid = $('.grid').isotope({
-        itemSelector: '.grid-item',
-        percentPosition: true,
-        masonry: {
-            // use outer width of grid-sizer for columnWidth
-            columnWidth: 1
-        }
-    })
 
 // filter items on button click
     $('.filter-button-group').on( 'click', 'button', function() {
@@ -25,6 +16,15 @@
 
     // COUNTER UP
     $('.counter').countUp();
+
+    //OWL CAROUSEL
+    $('.testimonial-activator').owlCarousel({
+        autoplay: true,
+        loop:true,
+        margin:15,
+        nav:false,
+        dots: true,
+    })
 
 })(jQuery);
 //ANIMATED PROGRESS BAR
@@ -48,19 +48,21 @@ function animate() {
 
 //LEAFLET JS MAPS
 function myMaps() {
-    var L = window.L;
-    var mymap = L.map('map',{ zoomControl: false }).setView([51.509865, -0.118092], 13);
+    var location = [51.505, -0.09];
+    var mapInit = L.map('map', {zoomControl: false}).setView(location, 13);
     var markerIcon = L.icon({
-        iconUrl: './img/marker_logo.png',
-        iconSize: [60, 60],
+        iconUrl: "./img/marker_logo.png",
+        iconSize: [100, 80],
     });
-    var marker = L.marker([51.509865, -0.118092], { icon: markerIcon }).addTo(mymap);
-    marker.bindPopup("<h3>Adrress:</h3> <p>450 Strand, Charing Cross, London WC2R 0RG, UK </p><h3>Phone:</h3>+44 20 7930 8205  ||  +44 20 7839 2323");
+    var marker = L.marker(location, {icon: markerIcon}).addTo(mapInit);
+    marker.bindPopup("<h3>Adrress:</h3> <p>450 Strand, Charing Cross, London WC2R 0RG, UK </p><h3>Phone:</h3>+44 20 7930 8205  ||  +44 20 7839 2323").openPopup();
 
-    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=4s5tSx5WUxHw28DRO8fi', {
-        attribution: '<a href="https://api.mapbox.com/styles/v1/devnazmul20/ckj2e1tph1ivs19mwgxmz3pc5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2bmF6bXVsMjAiLCJhIjoiY2tqMmRob29jMDJmZzJzazNpanVuZnk5ZiJ9.mywcRlchbp6szcofExXdtA" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank"></a>',
-        minZoom: 3,
+    L.tileLayer('https://api.mapbox.com/styles/v1/devnazmul20/ckj2jxo0k065619pbedx62e5r/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2bmF6bXVsMjAiLCJhIjoiY2tqMmRob29jMDJmZzJzazNpanVuZnk5ZiJ9.mywcRlchbp6szcofExXdtA', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
-    }).addTo(mymap);
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(mapInit);
 }
